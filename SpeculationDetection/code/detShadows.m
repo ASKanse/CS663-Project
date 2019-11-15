995254b969b2735e0ff546909690b193c4ddce5f
Aswin P Ajayan <aswinp2008@gmail.com>
2019-11-15 20:24:31 +0530

    added Specualtion mask as well

--- SpeculationDetection/code/detShadows.m
+++ SpeculationDetection/code/detShadows.m
@@ -10,14 +10,22 @@ YUV_F = rgb2ycbcr(imread('../../data/potsdetail_00_flash.tif'));
 FigH = uifigure ;
 %imshow(A,'parent',FigH.UIAxes);
 TextH = uicontrol('style','text','position',[170 340 40 15]);
-
-SliderH = uislider(FigH,'position',[50 50 150  3],'limits',[1 40], 'ValueChangedFcn', @(SliderH,event) callbackfn(SliderH,YUV_A,YUV_F));
+MASK_SHADOWS = zeros(size(YUV_A(:,:,1)));
+MASK_SPECULATIONS = YUV_F(:,:,1) > (0.90 * max(YUV_A(:)));
+se = exp(-(-20:20).^2/18);
+se = se'*se;
+MASK_SPECULATIONS = double(MASK_SPECULATIONS);
+%MASK_SPECULATIONS = imdilate(MASK_SPECULATIONS,se);
+MASK_SPECULATIONS = imfilter(MASK_SPECULATIONS,se);
+MASK_SPECULATIONS = (MASK_SPECULATIONS>(max(MASK_SPECULATIONS(:)) + MASK_SPECULATIONS>min(MASK_SPECULATIONS(:))/2)) ;
+figure; imshow(MASK_SPECULATIONS);
+SliderH = uislider(FigH,'position',[50 50 150  3],'limits',[1 40], 'ValueChangedFcn', @(SliderH,event) callbackfn(SliderH,YUV_A,YUV_F,MASK_SHADOWS));
 %addlistener(SliderH, 'Value', 'PostSet', @callbackfn);
 movegui(FigH, 'center')
-function callbackfn(SliderH,A,F)
+function callbackfn(SliderH,A,F,MASK_SHADOWS)
     num          = SliderH.Value;
     TextH.String = num2str(num);
 %imshow(((F-A)>num));
-M = (F(:,:,1)-A(:,:,1)) < num;
-imshow(M);
+MASK_SHADOWS = (F(:,:,1)-A(:,:,1)) < num;
+imshow(MASK_SHADOWS);
 end
